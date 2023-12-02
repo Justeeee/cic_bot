fdc62a50cb42b65fdafb68f7b9b5d3bea4f41c2e
Nozima Mirakbarova <nmirakbarova03@gmail.com>
2023-12-02 17:56:25 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,34 +5,41 @@ import aiogram
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
+import time
 
 
-
-bot = Bot(token="6637408089:AAEewGo8LALefLK9mQ9dGR8pCHnKX00WvD8")
+bot = Bot(token="6530826034:AAFlQ-j5q0ohOc_RwKlxPwHwg0WyljSMRrQ")
 dp = Dispatcher(bot)
 
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
-    await message.reply("Привет!\nНапиши мне свою жалобу или же просьбу, и я отправлю это Студенческому Совету!")
+    await message.reply("Welcome to the Student Union Bot!\nWe are here to help you with your requests and complaints. Please feel free to type your query and we will do our best to assist you.")
 
 
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
-    await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст Студенческому Совету!")
+    await message.reply("Write me something and I will send it to the Students Council!")
 
 
 @dp.message_handler()
 async def echo_message(msg: types.Message):
     current_time = datetime.now().strftime("%H:%M:%S")
     await bot.send_message(1679253464, f"New message \n"
-                                       f"Time : {current_time}\n"
+                                       f"Time : {current_time}"
                                        f"First name : {msg.from_user.first_name}\n"
                                        f"Last name: {msg.from_user.last_name}\n"
                                        f"ID : {msg.from_user.id}\n"
                                        f"Username @{msg.from_user.username}\n"
                                        f"Text : {msg.text}")
-    await bot.send_message(msg.from_user.id, "Успешно отправлено! Если нас заинтересует твоя идея или же проблема мы свяжемся с тобой!")
+    await bot.send_message(1669864103, f"New message \n"
+                                       f"Time : {current_time}"
+                                       f"First name : {msg.from_user.first_name}\n"
+                                       f"Last name: {msg.from_user.last_name}\n"
+                                       f"ID : {msg.from_user.id}\n"
+                                       f"Username @{msg.from_user.username}\n"
+                                       f"Text : {msg.text}")
+    await bot.send_message(msg.from_user.id, "Your message was sent successfully!")
 
 
 if __name__ == '__main__':
