b2c3bc206df3966fd23a8f944dd3f85187e07c51
Nozima Mirakbarova <nmirakbarova03@gmail.com>
2023-12-02 17:42:38 +0500

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ async def process_help_command(message: types.Message):
 async def echo_message(msg: types.Message):
     current_time = datetime.now().strftime("%H:%M:%S")
     await bot.send_message(1679253464, f"New message \n"
-                                       f"Time : {current_time}"
+                                       f"Time : {current_time}\n"
                                        f"First name : {msg.from_user.first_name}\n"
                                        f"Last name: {msg.from_user.last_name}\n"
                                        f"ID : {msg.from_user.id}\n"
